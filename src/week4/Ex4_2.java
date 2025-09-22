e031f7d1010ece3172a0f7271f4f6780f8d5de24
sungwoooooo <abc1239kkk@naver.com>
2025-09-22 15:05:14 +0900

    4주차 예제

---
+++ src/week4/Ex4_2.java
@@ -0,0 +1,27 @@
+//Rectangle 클래스 만들기 연습
+//너비와 높이를 입력 받아 사각형의 합을 출력하는 프로그램을 작성
+//너비와 높이 필드, 그리곡 면적 값을 제공하는 getArea() 메소드를 가진 Rectangle 클래스를 만들어 활용하라.
+package week4;
+
+import java.util.Scanner;
+
+public class Ex4_2 { //Rectangle
+    int width;
+    int height;
+
+    public int getArea() {
+        return width * height;
+    }
+
+    public static void main(String[] args) {
+        Ex4_2 rect = new Ex4_2();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(">> ");
+        rect.width = scanner.nextInt();
+        rect.height = scanner.nextInt();
+
+        System.out.print("사각형의 면적은 " + rect.getArea());
+
+        scanner.close();
+    }
+}
---
+++ src/week4/Ex4_7.java
@@ -0,0 +1,38 @@
+//객체 배열 만들기 연습
+/*
+    예제 4-4의 Book 클래스를 활용하여 2개짜리 Book 객체 배열을 만들고,
+    사용자로부터 책의 제목과 저자를 입력 받아 배열을 완성하라.
+*/
+package week4;
+
+import java.util.Scanner;
+
+class Book {
+    String title, author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+}
+
+public class Ex4_7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Book[] book = new Book[2];
+        for (int i = 0; i < book.length; i++) { //magin number 사용
+            System.out.print("제목>>");
+            String name = scanner.nextLine();
+            System.out.print("저자>>");
+            String author = scanner.nextLine();
+            book[i] = new Book(name, author);
+        }
+
+        for (int i = 0; i < book.length; i++) {
+            System.out.print("(" + book[i].title + ", " + book[i].author + ")");
+        }
+    }
+}
+
+
