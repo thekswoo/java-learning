051076b84af0b817cca86bcdc02a5ab356a55e3a
sungwoooooo <abc1239kkk@naver.com>
2025-09-29 12:22:12 +0900

    Ex5_1 예제 업로드

---
+++ src/week5/Ex5_1.java
@@ -0,0 +1,38 @@
+package week5;
+
+class Point {
+    private int x, y;
+
+    public void set(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void showPoint() {
+        System.out.println("(" + x + "," + y + ")");
+    }
+}
+
+class ColorPoint extends Point {
+    private String color;
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void showColorPoint() {
+        System.out.print(color);
+        showPoint();
+    }
+}
+
+public class Ex5_1 {
+    public static void main(String[] args) {
+        Point p = new Point();
+        p.set(1,2);
+        p.showPoint();
+
+        ColorPoint cp = new ColorPoint();
+        cp.set(3,4); //Point 부모class의 메소드 호출
+        cp.setColor("red");
+        cp.showColorPoint();
+    }
+}
