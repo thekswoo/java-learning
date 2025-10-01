35e8d62f221b692b50f85d383c2f470125c535e3
sungwoooooo <abc1239kkk@naver.com>
2025-10-01 12:38:28 +0900

    5-2 예제 코드

---
+++ src/week5/Ex5_8.java
@@ -0,0 +1,46 @@
+//인터페이스 구현
+/*
+    PhoneInterface 인터페이스를 구현하고
+    flash() 메소드를 추가한 SamsungPhone 클래스를 작성하라.
+ */
+
+package week5;
+
+interface PhoneInterface {
+    final int TIMEOUT = 10000; //상수 field
+
+    void sendCall(); //abstract method
+
+    void receiveCall(); //abstract method
+
+    default void printLogo() { //default method
+        System.out.println("** Phone **");
+    }
+}
+
+class SamsungPhone implements PhoneInterface {
+    @Override
+    public void sendCall() {
+        System.out.println("띠리리리링");
+    }
+
+    @Override
+    public void receiveCall() {
+        System.out.println("전화가 왔습니다.");
+    }
+
+    //add new method
+    public void flash() {
+        System.out.println("전화기에 불이 켜졌습니다.");
+    }
+}
+
+public class Ex5_8 {
+    public static void main(String[] args) {
+        SamsungPhone phone = new SamsungPhone();
+        phone.printLogo();
+        phone.sendCall();
+        phone.receiveCall();
+        phone.flash();
+    }
+}
