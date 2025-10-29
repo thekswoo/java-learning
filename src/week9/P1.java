a9ccc1b71037cf3e4cf466d07d860dfe28356c70
thekswoo <thekswoo@gmail.com>
2025-10-29 12:15:13 +0900

    feat: 9주차 예제 코드
    9주차 예제코드 8-1~3

---
+++ src/week9/P1.java
@@ -0,0 +1,7 @@
+package week9;
+
+public class P1 {
+    public static void main(String []args){
+
+    }
+}
---
+++ src/week9/P2.java
@@ -0,0 +1,7 @@
+package week9;
+
+public class P2 {
+    public static void main(String []args){
+
+    }
+}
