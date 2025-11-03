a0dde6e43a62fe02e8aa82d6b0092bac2f28d80a
thekswoo <thekswoo@gmail.com>
2025-11-03 15:44:33 +0900

    feat: Ex9_4 생성
    BoardLayout 배치관리자를 사용하는 예

---
+++ src/chap09_gui_swing/Ex9_4.java
@@ -0,0 +1,32 @@
+//BoardLayout 배치관리자를 사용하는 예
+/*
+    BoardLayout 배치관리자를 사용하여 컨텐츠팬에 다음과 같이 5개의 버튼 컴포넌트를 삽입하라.
+ */
+
+package chap09_gui_swing;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Ex9_4 extends JFrame {
+    public Ex9_4() {
+        setTitle("BorderLayout Sample");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container c = getContentPane();
+
+        c.setLayout(new BorderLayout(30,20));
+        c.add(new JButton("Calculate"), BorderLayout.CENTER);
+        c.add(new JButton("add"),BorderLayout.NORTH);
+        c.add(new JButton("sub"),BorderLayout.SOUTH);
+        c.add(new JButton("mul"),BorderLayout.EAST);
+        c.add(new JButton("div"),BorderLayout.WEST);
+
+        setSize(300,200);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Ex9_4();
+    }
+}
