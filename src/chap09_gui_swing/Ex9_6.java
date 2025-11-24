e3d087f5863d245c1d674ae35daf0c7db0256833
kangsungwoo <thekswoo@gmail.com>
2025-11-24 12:14:58 +0900

    docs: 코드 설명 추가

--- src/chap09_gui_swing/Ex9_6.java
+++ src/chap09_gui_swing/Ex9_6.java
@@ -13,14 +13,14 @@ public class Ex9_6 extends JFrame {
         c.setLayout(null);
 
         JLabel la = new JLabel("Hello, Press Buttons!");
-        la.setLocation(130,50);
-        la.setSize(200,20);
+        la.setLocation(130,50); // 절대 위치 조정
+        la.setSize(200,20); // 절대 크기 조정
         c.add(la);
 
         for(int i=1; i<=9; i++){
-            JButton b = new JButton(Integer.toString(i)); //버튼 생성
-            b.setLocation(i*15, i*15);
-            b.setSize(50,20);
+            JButton b = new JButton(Integer.toString(i)); // 버튼 생성
+            b.setLocation(i*15, i*15); // 절대위치 조정
+            b.setSize(50,20); // 절대크기 조정
             c.add(b);
         }
         setSize(300,200);
